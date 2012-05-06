395209dc0f7cd610e196b07657620272aedbbcb8
Tom Denley <t.denley@cantab.net>
2012-05-06 10:55:07 +0100

    do that samba shake

--- backup.sh
+++ backup.sh
@@ -9,7 +9,21 @@ LOGFILE="$HOME"/mybackup.log
 
 /bin/echo "Nightly Backup Started: $(date)" >> $LOGFILE
 
+# mount samba shares
+/bin/rm -fr "$HOME"/.smb
 /bin/cp -R "$WRKDIR"/smb "$HOME"/.smb 
+/bin/cp "$HOME"/smbnetfs.auth "$HOME"/.smb
+/bin/chmod 600 "$HOME"/.smb/*
+
+/bin/mkdir -p "$HOME"/samba
+/usr/bin/smbnetfs "$HOME"/samba
+
+ls "$HOME"/samba/phlox.lan/tom
+
+# unmount samba shares
+/bin/fusermount -u "$HOME"/samba
+/bin/rm -fr "$HOME"/samba
+
 /usr/local/bin/u1sync --oauth="$U1OAUTH" --action=upload "$U1DIR"
 
 /bin/echo "Nightly Backup Successful: $(date)" >> "$LOGFILE"
