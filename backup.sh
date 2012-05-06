02dc2b662ced00bf88e6b2dfb012b08fa43b7560
Tom Denley <t.denley@cantab.net>
2012-05-06 09:06:54 +0100

    typos

--- backup.sh
+++ backup.sh
@@ -3,12 +3,12 @@ set -u
 set -e
 
 U1DIR=$HOME/u1
-U1OATH=`cat $HOME/u1oath.key`
+U1OAUTH=`cat $HOME/u1oauth.key`
 LOGFILE=$HOME/backup.log
 
 
 echo "Nightly Backup Started: $(date)" >> $LOGFILE
 
-/usr/local/bin/u1sync --oauth=$U1AUTH --action=upload $U1DIR
+/usr/local/bin/u1sync --oauth=$U1OAUTH --action=upload $U1DIR
 
 echo "Nightly Backup Successful: $(date)" >> $LOGFILE
