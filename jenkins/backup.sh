0c7a6bb5976aa6a723ef935bf26e5218b582304b
Tom Denley <t.denley@cantab.net>
2012-11-19 20:33:32 +0000

    Update jenkins/backup.sh

--- jenkins/backup.sh
+++ jenkins/backup.sh
@@ -13,6 +13,6 @@ then
   exit 1;
 fi
 
-SIZE="$( git ls-files -m -o --exclude-standard | while read f; do du "$f"; done | awk 'BEGIN {t=0} {t += $1} END {print t}' )"
+SIZE="$( git ls-files -m -o --exclude-standard | while read f; do du -b "$f"; done | awk 'BEGIN {t=0} {t += $1} END {print t}' )"
 
 echo $SIZE
