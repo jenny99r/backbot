988714e242ac803b3f82ea4c5c6cc8fcd1da0af3
scarytom <t.denley@cantab.net>
2012-12-02 12:30:47 +0000

    simple scanning done

--- scanning/scan.sh
+++ scanning/scan.sh
@@ -9,11 +9,9 @@ OUTFILE="$HOME"/scan-$(date -u +%Y%m%d%H%M%S).tif
 echo "Scanning Started: $(date)"
 
 cd "$TMPDIR"
-scanimage --mode "Color" --device-name "fujitsu:ScanSnap S1500:94374" -y 297 -x 210 --page-width 210 --page-height 297 --batch --source "ADF Duplex" --resolution 180 --format=tiff
+scanimage --mode "Color" --device-name "fujitsu:ScanSnap S1500:94374" -y 297 -x 210 --page-width 210 --page-height 297 --batch --source "ADF Duplex" --resolution 180 --format=tiff && echo ""
 
-tiffcp "out*.tif" "combined.tif"
-
-cp "combined.tif" "$OUTFILE"
+tiffcp "$TMPDIR"/out*.tif "$OUTFILE"
 rm -fr "$TMPDIR"
 
 echo "Scanning Finished: $(date)"
