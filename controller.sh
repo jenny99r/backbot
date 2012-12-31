961d7aa8c22b293e065ff2565bf01791c0fc9f77
scarytom <t.denley@cantab.net>
2012-12-31 10:28:26 +0000

    trial scanning on button press

--- controller.sh
+++ controller.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-
+set -u
 set -e
+
+SCRIPT_DIR="$( cd "$( /usr/bin/dirname "${BASH_SOURCE[0]}" )" && /bin/pwd )"
 
 gpio-admin export 22
 
-while [ `cat /sys/devices/virtual/gpio/gpio22/value` = 1 ]; do
-  sleep 0.05
+while : do
+  while [ `cat /sys/devices/virtual/gpio/gpio22/value` = 1 ]; do
+    sleep 0.05
+  done
+  "$SCRIPT_DIR"/scan.sh
 done
-
-
