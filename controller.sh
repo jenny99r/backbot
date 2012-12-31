1ae5772c9fffdab8358eaaad6e0c5107861b2ee8
scarytom <t.denley@cantab.net>
2012-12-31 10:37:41 +0000

    typo

--- controller.sh
+++ controller.sh
@@ -6,7 +6,8 @@ SCRIPT_DIR="$( cd "$( /usr/bin/dirname "${BASH_SOURCE[0]}" )" && /bin/pwd )"
 
 gpio-admin export 22
 
-while : do
+while :
+do
   while [ `cat /sys/devices/virtual/gpio/gpio22/value` = 1 ]; do
     sleep 0.05
   done
