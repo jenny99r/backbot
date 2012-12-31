81db9d1ddb6e6e87e4e9f216ee909f772e08a01b
scarytom <t.denley@cantab.net>
2012-12-31 10:45:20 +0000

    scan script

--- scan.sh
+++ scan.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-set -e
 set -u
+set -e
 
-echo "woot"
+SCRIPT_DIR="$( cd "$( /usr/bin/dirname "${BASH_SOURCE[0]}" )" && /bin/pwd )"
+OUTFILE="$HOME"/scan-$(date -u +%Y%m%d%H%M%S).tif
+TMPDIR=`mktemp -d`
+
+echo "Scanning Started: $(date)"
+
+cd "$TMPDIR"
+scanimage --mode "Color" --device-name "fujitsu:ScanSnap S1500:94374" -y 297 -x 210 --page-width 210 --page-height 297 --batch --source "ADF Duplex" --resolution 180 --format=tiff --sleeptimer 1 && echo ""
+
+tiffcp "$TMPDIR"/out*.tif "$OUTFILE"
+rm -fr "$TMPDIR"
+
+echo "Scanning Finished: $(date)"
