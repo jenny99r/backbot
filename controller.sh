3785451e8a1012ea1e36f0facb962b2faeaa521b
scarytom <t.denley@cantab.net>
2013-01-06 17:21:48 +0000

    update following some hardware... buttons, yay.

--- controller.sh
+++ controller.sh
@@ -4,12 +4,48 @@ set -e
 
 SCRIPT_DIR="$( cd "$( /usr/bin/dirname "${BASH_SOURCE[0]}" )" && /bin/pwd )"
 
-gpio-admin export 22
+PORT_POWER=14
+PORT_DUPLEX=4
+PORT_SCAN_SINGLE=0
+PORT_SCAN_MULTIPLE=1
+
+gpio-admin export $PORT_POWER
+gpio-admin export $PORT_DUPLEX
+gpio-admin export $PORT_SCAN_SINGLE
+gpio-admin export $PORT_SCAN_MULTIPLE
+
+read() {
+  return `cat /sys/devices/virtual/gpio/gpio$1/value`
+}
+
+is_duplex() {
+  if read $DUPLEX;
+  then
+    return "duplex"
+  fi
+  return "simplex"
+}
+
+scan() {
+  "$SCRIPT_DIR"/scan.sh $1 is_duplex
+}
 
 while :
 do
-  while [ `cat /sys/devices/virtual/gpio/gpio22/value` = 1 ]; do
-    sleep 0.05
+  while ! ( read $PORT_SCAN_SINGLE || read $PORT_SCAN_MULTIPLE || read $PORT_POWER ); do
+    sleep 0.02
   done
-  "$SCRIPT_DIR"/scan.sh
+
+  if read $PORT_POWER;
+  then
+    echo "powering down"
+    exit 0;
+  elif read $PORT_SCAN_SINGLE;
+  then
+    echo "scanning single"
+  elif read $PORT_SCAN_MULTIPLE;
+  then
+    echo "scanning multiple"
+  fi
 done
+
--- scan.sh
+++ scan.sh
@@ -5,16 +5,32 @@ set -e
 SCRIPT_DIR="$( cd "$( /usr/bin/dirname "${BASH_SOURCE[0]}" )" && /bin/pwd )"
 OUTFILE="$HOME"/scan-$(date -u +%Y%m%d%H%M%S).tif
 TMPDIR=`mktemp -d`
+SINGLE=0
+SOURCE="ADF Duplex"
 
 echo "Scanning Started: $(date)"
 
+if [ $1 = "multiple" ];
+then
+  SINGLE=1
+fi;
+
+if [ $2 = "simplex" ];
+then
+  SOURCE="ADF Front"
+fi;
+
 cd "$TMPDIR"
-scanimage --mode "Color" --device-name "fujitsu:ScanSnap S1500:94374" -y 297 -x 210 --page-width 210 --page-height 297 --batch --source "ADF Duplex" --resolution 180 --format=tiff --sleeptimer 1 && echo ""
+scanimage --mode "Color" --device-name "fujitsu:ScanSnap S1500:94374" -y 297 -x 210 --page-width 210 --page-height 297 --batch --source "$SOURCE" --resolution 180 --format=tiff --sleeptimer 1 && echo ""
 
 if [ ! -f "$TMPDIR/out*.tif" ]; then
   echo "nothing was scanned"
-else
+elif $SINGLE
   tiffcp "$TMPDIR"/out*.tif "$OUTFILE"
+else
+  for file in "$TMPDIR"/out*.tif ; do
+    mv "$file" "${OUTFILE%.*}-${file#out}"
+  done
 fi
 
 rm -fr "$TMPDIR"
