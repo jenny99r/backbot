895e11240667e6e90858403903fb5470f15eaee5
Tom Denley <t.denley@cantab.net>
2013-01-15 18:35:20 +0000

    remove backup and establish master as the jenkins configuration and setup

--- setup/crontab.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -u
-set -e
-
-BACKBOTDIR="$( cd "$( /usr/bin/dirname "${BASH_SOURCE[0]}" )" && cd .. && /bin/pwd )"
-
-echo "Setting up crontab"
-crontab -r || echo ""
-echo "01 03 * * * $BACKBOTDIR/backup.sh" | crontab -  
