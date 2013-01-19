11314268fe102cdaec0cc9e173d7e6023a53bf0a
scarytom <t.denley@cantab.net>
2013-01-19 17:53:07 +0000

    scrub install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,3 @@
 set -u
 set -e
 
-BACKBOTDIR="$( cd "$( /usr/bin/dirname "${BASH_SOURCE[0]}" )" && cd .. && /bin/pwd )"
-
-echo "Setting up crontab"
-crontab -r || echo ""
-echo "01 03 * * * $BACKBOTDIR/backup.sh" | crontab -  
